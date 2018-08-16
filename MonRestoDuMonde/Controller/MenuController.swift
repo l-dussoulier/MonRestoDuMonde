827b5324e23972c20459a445e4cf57aa9c5dbc46
DUSSOULIER <dussoulier.louis@hotmail.fr>
2018-08-16 18:42:03 +0200

    Ajout des controller principaux

---
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by DUSSOULIER on 16/08/2018.
+//  Copyright © 2018 DUSSOULIER. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+}
