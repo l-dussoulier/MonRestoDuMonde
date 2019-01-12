4dae8dcbea147e48e53d43074f0b292f9540c65f
DUSSOULIER <dussoulier.louis@hotmail.fr>
2019-01-12 16:19:21 +0100

    ajout collection view

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by DUSSOULIER on 12/01/2019.
+//  Copyright © 2019 DUSSOULIER. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+    
+    
+}
