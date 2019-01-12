8fb0e8fdc7b3cf8260b932b3c7ee51a779b39df6
DUSSOULIER <dussoulier.louis@hotmail.fr>
2019-01-12 16:30:42 +0100

    Ajout classe Plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,14 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by DUSSOULIER on 12/01/2019.
+//  Copyright © 2019 DUSSOULIER. All rights reserved.
+//
+
+import Foundation
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
